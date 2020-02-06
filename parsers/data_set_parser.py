9ac7763d275b4af6523164707c23250823878dce
Joshua Koopmans <joshua.abc@hotmail.com>
2020-02-06 13:04:00 +0100

    Added files and more protein stuff

--- parsers/data_set_parser.py
+++ parsers/data_set_parser.py
@@ -2,10 +2,10 @@
 #parse_proteins() # dict NO_SP and SP
 
 def main():
-    open_and_read_file()
-
+    proteins = open_and_read_file()
+    parse_proteins(proteins)
 def open_and_read_file():
-    with open("/home/michelle/Downloads/SignalP/train_set.fasta") as file: #TODO: include file in git
+    with open("../resources/train_set.fasta.txt") as file: #TODO: include file in git
         proteins = []
         protein = ""
         for line in file:
@@ -15,6 +15,7 @@ def open_and_read_file():
                     protein = ""
             protein += line
         proteins.append(protein) #add last protein
+    print(proteins[0])
     return proteins
 
 def parse_proteins(proteins):
@@ -23,6 +24,16 @@ def parse_proteins(proteins):
     peptides["no_sp"] = []
     peptides["sp"] = []
     for protein in proteins:
+        identifier, sequence, metadata = protein.split("\n")[:-1]
+        if "|SP|" in identifier:
+            peptides["no_sp"] ##
+        elif "|NO_SP|" in identifier:
+            peptides["sp"] ##
+
+        print(identifier)
+        print(sequence)
+        print(metadata)
+
         
 #get_signal_proteins() # list with signal sequences
 #get_no_signal_proteins()
