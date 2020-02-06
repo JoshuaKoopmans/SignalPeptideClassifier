2fd092fac0a230f177ae789b967c2c866f6d8896
MichelledeGroot <michelle@rdegroot.net>
2020-02-06 13:44:28 +0100

    parse proteins start

--- parsers/data_set_parser.py
+++ parsers/data_set_parser.py
@@ -15,9 +15,15 @@ def open_and_read_file():
                     protein = ""
             protein += line
         proteins.append(protein) #add last protein
+    return proteins
 
-
-
+def parse_proteins(proteins):
+    #Dict {no_sp, sp}
+    peptides = {}
+    peptides["no_sp"] = []
+    peptides["sp"] = []
+    for protein in proteins:
+        
 #get_signal_proteins() # list with signal sequences
 #get_no_signal_proteins()
 
