852bbdd46c2a8197ebfdcd00887ef8daba9ec7c7
Joshua Koopmans <joshua.abc@hotmail.com>
2020-02-13 11:33:14 +0100

    Parser: dictionary with protein objects

--- parsers/data_set_parser.py
+++ parsers/data_set_parser.py
@@ -1,41 +1,35 @@
 #open_and_read_file() # return string
 #parse_proteins() # dict NO_SP and SP
-
+import domain.Signal_class as sc
 def main():
     proteins = open_and_read_file()
     parse_proteins(proteins)
 def open_and_read_file():
     with open("../resources/train_set.fasta.txt") as file: #TODO: include file in git
-        proteins = []
-        protein = ""
+        entries = []
+        entry = ""
         for line in file:
             if line.startswith(">"):
-                if protein != "":
-                    proteins.append(protein)
-                    protein = ""
-            protein += line
-        proteins.append(protein) #add last protein
-    print(proteins[0])
-    return proteins
+                if entry != "":
+                    entries.append(entry)
+                    entry = ""
+            entry += line
+        entries.append(entry) #add last entry
+    print(entries[0])
+    return entries
 
-def parse_proteins(proteins):
-    #Dict {no_sp, sp}
+def parse_proteins(entries):
     peptides = {}
     peptides["no_sp"] = []
     peptides["sp"] = []
-    for protein in proteins:
-        identifier, sequence, metadata = protein.split("\n")[:-1]
+    for entry in entries:
+        identifier, sequence, metadata = entry.split("\n")[:-1]
         if "|SP|" in identifier:
-            peptides["no_sp"] ##
+            peptides["sp"].append(sc.Signal_proteins(identifier,sequence,metadata,True))
         elif "|NO_SP|" in identifier:
-            peptides["sp"] ##
+            peptides["no_sp"].append(sc.Signal_proteins(identifier,sequence,metadata,False))
 
-        print(identifier)
-        print(sequence)
-        print(metadata)
 
-        
-#get_signal_proteins() # list with signal sequences
-#get_no_signal_proteins()
+
 
 main()
